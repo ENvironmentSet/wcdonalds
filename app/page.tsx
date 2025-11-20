eaba375c9db74eb834f376b98253ee497410f8da
ENvironmentSet <sbshw1@gmail.com>
2025-11-20 13:26:58 +0900

    main intro

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,23 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+  const router = useRouter();
+
   return (
-    <h1>울랄라~</h1>
+    <video src="/assets/intro.mp4" playsInline width="100%" height="100%" ref={video => {
+      if (!video) return;
+
+      const handleClick = () => {
+        video.play();
+      }
+      
+      video.addEventListener('click', handleClick);
+
+      return () => video.removeEventListener('click', handleClick);
+    }} onEnded={
+      () => router.push('/hallucination')
+    } />
   )
 }
